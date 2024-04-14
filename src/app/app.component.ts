2a3c91de4c8b5b522692d35ba679b39f9fc41060
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2024-04-13 18:26:24 -0600

    Logica para el funcionamiento del spinner
    Logica para el funcionamiento del spinner en el sitio web

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isLoading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 2000);
+      }
+    });
+  }
+}
